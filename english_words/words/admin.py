8b43a4f33fc6a272b63a024834edcff2e6135f7f
Oleg Prikhodko <4.oleg.pr@gmail.com>
2022-12-13 21:25:15 +0600

    refactor: delete model Users, setup Admin panel

--- english_words/words/admin.py
+++ english_words/words/admin.py
@@ -1,3 +1,26 @@
 from django.contrib import admin
+from .models import Categories, Dictionaries, Words
 
-# Register your models here.
+
+class CategoriesAdmin(admin.ModelAdmin):
+    list_display = ('name', )
+    list_display_links = ('name', )
+    search_fields = ('name', )
+
+
+class DictionariesAdmin(admin.ModelAdmin):
+    list_display = ('name', 'url')
+    list_display_links = ('name', 'url')
+    search_fields = ('name', 'url')
+
+
+class WordsAdmin(admin.ModelAdmin):
+    list_display = ('word', 'time_create', 'time_clear', 'category_id')
+    list_display_links = ('word', )
+    search_fields = ('word', )
+    list_filter = ('time_create', 'time_clear')
+
+
+admin.site.register(Categories, CategoriesAdmin)
+admin.site.register(Dictionaries, DictionariesAdmin)
+admin.site.register(Words, WordsAdmin)
--- english_words/words/models.py
+++ english_words/words/models.py
@@ -1,25 +1,29 @@
 from django.db import models
 
 
-class Users(models.Model):
-    login = models.CharField(max_length=50, unique=True)
-    first_name = models.CharField(max_length=100, blank=True)
-    last_name = models.CharField(max_length=100, blank=True)
-
-
 class Categories(models.Model):
     name = models.CharField(max_length=100, unique=True)
 
-
-class Words(models.Model):
-    word = models.CharField(max_length=200, unique=True)
-    time_create = models.DateTimeField(auto_now_add=True)
-    time_clear = models.DateTimeField(blank=True)
-    user_id = models.ForeignKey('Users', on_delete=models.PROTECT)
-    category_id = models.ForeignKey('Categories', on_delete=models.PROTECT)
+    class Meta:
+        verbose_name = 'Category'
+        verbose_name_plural = 'Categories'
 
 
 class Dictionaries(models.Model):
     name = models.CharField(max_length=200, unique=True)
     url = models.DateTimeField(auto_now_add=True)
-    user_id = models.ForeignKey('Users', on_delete=models.PROTECT)
+
+    class Meta:
+        verbose_name = 'Dictionary'
+        verbose_name_plural = 'Dictionaries'
+
+
+class Words(models.Model):
+    word = models.CharField(max_length=200, unique=True)
+    time_create = models.DateTimeField(auto_now_add=True)
+    time_clear = models.DateTimeField(null=True)
+    category_id = models.ForeignKey('Categories', on_delete=models.PROTECT)
+
+    class Meta:
+        verbose_name = 'Word'
+        verbose_name_plural = 'Words'

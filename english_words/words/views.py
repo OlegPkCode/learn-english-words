e08950508d2c9ba759bbaf80db5f4ac9ed5fb585
Oleg Prikhodko <4.oleg.pr@gmail.com>
2022-12-14 23:33:24 +0600

    feat: create menu, create base tamplate  and update template index.html

--- english_words/words/views.py
+++ english_words/words/views.py
@@ -1,5 +1,10 @@
 from django.shortcuts import render
+from .models import Words
+
+menu = ["Изучаемые слова", "Статистика",
+        "Настройки", "Обратная связь", "Войти"]
 
 
 def index(request):
-    return render(request, 'words/index.html', {'title': 'words'})
+    words_list = Words.objects.all()
+    return render(request, 'words/index.html', {'menu': menu, 'title': 'words', 'words': words_list})

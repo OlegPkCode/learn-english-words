f08b28a7476fcfd98b4b196e719030f25749c535
Oleg Prikhodko <4.oleg.pr@gmail.com>
2022-12-22 19:24:01 +0600

    feat: delete and clearing word

--- english_words/words/forms.py
+++ english_words/words/forms.py
@@ -6,3 +6,14 @@ class AddWordForm(forms.ModelForm):
     class Meta:
         model = Words
         fields = ['word', 'category_id']
+
+
+# class ClearWordForm(forms.ModelForm):
+#     class Meta:
+#         model = Words
+#         fields = ['time_clear']
+
+# class ClearWordForm(forms.ModelForm):
+#     class Meta:
+#         model = Words
+#         fields = ['time_clear']
--- english_words/words/views.py
+++ english_words/words/views.py
@@ -1,3 +1,5 @@
+import datetime
+
 from django.shortcuts import render, redirect
 from .models import Words
 from .forms import AddWordForm
@@ -29,9 +31,18 @@ def add_word(request):
 
 
 def view_word(request, word_id):
-    word_name = Words.objects.get(pk=word_id)
+    word_item = Words.objects.get(pk=word_id)
 
-    word_data = {'word_id': word_id, 'word_name': word_name}
-    context = {'menu': menu, 'title': 'Слово', 'word_data': word_data}
+    if 'clear_word' in request.POST:
+        w = Words.objects.get(pk=word_id)
+        w.time_clear = datetime.datetime.now()
+        w.save()
+        return redirect('add_word')
+    elif 'delete_word' in request.POST:
+        w = Words.objects.get(pk=word_id)
+        w.delete()
+        return redirect('add_word')
+
+    context = {'menu': menu, 'title': 'Слово', 'word_item': word_item}
 
     return render(request, 'words/view_word.html', context=context)

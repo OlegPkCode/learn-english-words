b716708bce06b62ec097db9235c7f32259db0830
Oleg Prikhodko <4.oleg.pr@gmail.com>
2022-12-23 19:14:32 +0600

    refactor: add bootstrap

--- english_words/words/urls.py
+++ english_words/words/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 
-from words.views import add_word, view_word, confirm_delete_word
+from words.views import add_word, view_word, confirm_delete_word, learned_words
 
 urlpatterns = [
     path('', add_word, name='add_word'),
     path('view_word/<int:word_id>', view_word, name='view_word'),
     path('confirm_delete_word/<int:word_id>', confirm_delete_word, name='confirm_delete_word'),
+    path('learned_words/', learned_words, name='learned_words'),
 ]
--- english_words/words/views.py
+++ english_words/words/views.py
@@ -3,15 +3,9 @@ from django.shortcuts import render, redirect
 from .models import Words
 from .forms import AddWordForm
 
-menu = ["Изучаемые слова", "Статистика",
-        "Настройки", "Обратная связь", "Войти"]
-
 
 def add_word(request):
     words_list = Words.objects.filter(time_clear__isnull=True)
-    # words_list = Words.objects.all()
-    form = AddWordForm(initial={'category_id': 1})
-    form.fields['category_id'].initial = 1
 
     if request.method == 'POST':
         form = AddWordForm(request.POST)
@@ -22,13 +16,32 @@ def add_word(request):
         form = AddWordForm()
 
     context = {
-        'menu': menu,
         'title': 'Слова',
         'words': words_list,
         'form': form,
     }
 
     return render(request, 'words/add_word.html', context=context)
+
+
+def learned_words(request):
+    words_list = Words.objects.filter(time_clear__isnull=False)
+
+    if request.method == 'POST':
+        form = AddWordForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('add_word')
+    else:
+        form = AddWordForm()
+
+    context = {
+        'title': 'Слова',
+        'words': words_list,
+        'form': form,
+    }
+
+    return render(request, 'words/learned_words.html', context=context)
 
 
 def view_word(request, word_id):
@@ -42,7 +55,7 @@ def view_word(request, word_id):
     elif 'delete_word' in request.POST:
         return redirect('confirm_delete_word', word_id=word_id)
 
-    context = {'menu': menu, 'title': 'Слово', 'word_item': word_item}
+    context = {'title': 'Слово', 'word_item': word_item}
 
     return render(request, 'words/view_word.html', context=context)
 
@@ -57,6 +70,6 @@ def confirm_delete_word(request, word_id):
     elif 'cancel_delete' in request.POST:
         return redirect('add_word')
 
-    context = {'menu': menu, 'title': 'Слово', 'word_item': word_item}
+    context = {'title': 'Слово', 'word_item': word_item}
 
     return render(request, 'words/confirm_delete_word.html', context=context)

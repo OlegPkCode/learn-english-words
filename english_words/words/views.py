8164d7707b587ae514fceb7c8363ab7fd577cb4b
Oleg Prikhodko <4.oleg.pr@gmail.com>
2023-01-15 19:44:19 +0600

    feat: add change word

--- english_words/words/views.py
+++ english_words/words/views.py
@@ -1,6 +1,6 @@
 from datetime import datetime
 from django.shortcuts import render, redirect
-from .models import Words
+from .models import Words, Categories
 from .forms import AddWordForm
 
 
@@ -24,6 +24,40 @@ def add_word(request):
     return render(request, 'words/add_word.html', context=context)
 
 
+def view_word(request, word_id):
+    word_item = Words.objects.get(pk=word_id)
+    form = AddWordForm()
+
+    if 'clear_word' in request.POST:
+        if word_item.time_clear is None:
+            word_item.time_clear = datetime.now()
+        word_item.save()
+        return redirect('add_word')
+    elif 'study_word' in request.POST:
+        Words.objects.filter(pk=word_id).update(time_clear=None)
+        return redirect('add_word')
+    elif 'delete_word' in request.POST:
+        return redirect('confirm_delete_word', word_id=word_id)
+
+    if request.method == 'POST':
+        form = AddWordForm(request.POST)
+        if form.is_valid():
+            word_item.name = form.data['name']
+            word_item.category_id = Categories.objects.get(pk=form.data['category_id'])
+            word_item.save()
+            return redirect('add_word')
+    else:
+        form = AddWordForm()
+
+    context = {
+        'title': 'Слово',
+        'word_item': word_item,
+        'form': form,
+    }
+
+    return render(request, 'words/view_word.html', context=context)
+
+
 def learned_words(request):
     words_list = Words.objects.filter(time_clear__isnull=False)
 
@@ -44,28 +78,11 @@ def learned_words(request):
     return render(request, 'words/learned_words.html', context=context)
 
 
-def view_word(request, word_id):
-    word_item = Words.objects.get(pk=word_id)
-
-    if 'clear_word' in request.POST:
-        w = Words.objects.get(pk=word_id)
-        w.time_clear = datetime.now()
-        w.save()
-        return redirect('add_word')
-    elif 'delete_word' in request.POST:
-        return redirect('confirm_delete_word', word_id=word_id)
-
-    context = {'title': 'Слово', 'word_item': word_item}
-
-    return render(request, 'words/view_word.html', context=context)
-
-
 def confirm_delete_word(request, word_id):
     word_item = Words.objects.get(pk=word_id)
 
     if 'delete_word' in request.POST:
-        w = Words.objects.get(pk=word_id)
-        w.delete()
+        word_item.delete()
         return redirect('add_word')
     elif 'cancel_delete' in request.POST:
         return redirect('add_word')
